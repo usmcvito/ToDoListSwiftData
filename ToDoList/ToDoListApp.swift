62b35dbdd5fe60705d646d9b1b3206da8a407925
Vito <jegenovese@hotmail.com>
2024-12-08 06:07:32 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Vito on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
